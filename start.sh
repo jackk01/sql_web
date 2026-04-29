832348580832a5f5fe484795c6e64815e5623bab
longyang <longyang@leyoujia.com>
2026-04-29 17:48:43 +0800

    update

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 python3.11 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
-uvicorn app.main:app --reload
-
+# uvicorn app.main:app --reload
+python -m uvicorn app.main:app --reload
 
 npm install
 npm run dev
